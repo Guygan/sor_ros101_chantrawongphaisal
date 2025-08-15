c7f40245edada2bf2e6fa57aca052d1d414ccd9a
Guygan <guygan2002@gmail.com>
2025-08-15 15:11:55 +0700

    Assignment 4

--- my_robot_description/launch/gazebo.launch.py
+++ my_robot_description/launch/gazebo.launch.py
@@ -9,30 +9,24 @@ from launch_ros.parameter_descriptions import ParameterValue
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-    # Get the share directory of your package
     pkg_share_dir = get_package_share_directory('my_robot_description')
 
-    # Define the path to the single combined Xacro file
     urdf_path = os.path.join(pkg_share_dir, 'urdf', 'my_robot.urdf.xacro')
 
-    # Create the full robot description from the main Xacro file
     robot_description_content = ParameterValue(
         Command(['xacro', ' ', urdf_path]),
         value_type=str
     )
 
-    # Set environment variable for Gazebo resources (models, worlds)
-    # This helps Gazebo find models defined in your URDF.
+    # Set environment variable to help Gazebo find your robot's resources (like meshes if you add them later)
     set_env_var_resources = SetEnvironmentVariable(
         name='GZ_SIM_RESOURCE_PATH',
         value=os.path.join(pkg_share_dir, 'urdf')
     )
 
-    # --- KERNEL OF THE FIX ---
-    # Open Gazebo Sim, starting both the server and the GUI.
-    # We remove the '-s' (server-only) flag to allow the GUI to launch.
-    # The gz_sim.launch.py script starts both server and client by default
-    # when neither -s (server-only) nor -g (gui-only) is specified.
+    # --- FIX APPLIED HERE ---
+    # Launch Gazebo Sim, starting both the server and the GUI.
+    # The '-s' (server-only) flag has been REMOVED to allow the GUI to launch.
     gazebo_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([
             PathJoinSubstitution([
@@ -41,15 +35,12 @@ def generate_launch_description():
                 'gz_sim.launch.py'
             ])
         ]),
-        # The key change is removing '-s' from gz_args.
-        # The ('gui', 'true') argument was also removed as it is not a valid
-        # argument for gz_sim.launch.py and could cause confusion.
         launch_arguments={
-            'gz_args': '-r -v 4'  # '-r' to run simulation on start, '-v 4' for verbosity
+            # The world file 'empty.sdf' is now part of gz_args
+            'gz_args': '-r -v 4 empty.sdf'
         }.items()
     )
 
-    # Run robot_state_publisher node
     robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
@@ -60,7 +51,6 @@ def generate_launch_description():
         }]
     )
 
-    # Spawn the robot in Gazebo
     spawn_robot = Node(
         package='ros_gz_sim',
         executable='create',
@@ -72,12 +62,14 @@ def generate_launch_description():
         output='screen'
     )
 
-    # Create a bridge for LiDAR data
+    # --- FIX APPLIED HERE ---
+    # Create a bridge for LiDAR data with corrected syntax.
+    # A closing bracket ']' has been added.
     lidar_bridge = Node(
         package='ros_gz_bridge',
         executable='parameter_bridge',
         arguments=[
-            '/world/default/model/my_robot/link/lidar_link/sensor/lidar/scan@sensor_msgs/msg/LaserScan[gz.msgs.LaserScan'
+            '/world/default/model/my_robot/link/lidar_link/sensor/lidar/scan@sensor_msgs/msg/LaserScan[gz.msgs.LaserScan]'
         ],
         remappings=[
             ('/world/default/model/my_robot/link/lidar_link/sensor/lidar/scan', '/scan')
@@ -85,7 +77,6 @@ def generate_launch_description():
         output='screen'
     )
 
-    # Node to convert LaserScan to Range (Sonar)
     laser_to_sonar_node = Node(
         package='my_robot_description',
         executable='laser_to_sonar',
--- my_robot_description/setup.py
+++ my_robot_description/setup.py
@@ -1,13 +1,13 @@
 import os
 from glob import glob
-from setuptools import setup, find_packages # เพิ่ม find_packages
+from setuptools import setup, find_packages
 
 package_name = 'my_robot_description'
 
 setup(
     name=package_name,
     version='0.0.1',
-    packages=find_packages(include=[package_name, package_name + '.*']), # บอกให้ setuptools ค้นหาและติดตั้ง Python package
+    packages=find_packages(include=[package_name, package_name + '.*']),
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
@@ -17,6 +17,7 @@ setup(
         (os.path.join('share', package_name, 'urdf'), glob(os.path.join('urdf', '*.xacro'))),
         # รวมไฟล์ RViz config (ถ้ามี)
         (os.path.join('share', package_name, 'rviz'), glob(os.path.join('rviz', '*.rviz'))),
+        # ✅ ลบบรรทัดนี้ออกไป: (os.path.join('share', package_name, 'worlds'), glob(os.path.join('worlds', '*.sdf'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -27,7 +28,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'laser_to_sonar = my_robot_description.laser_to_sonar_node:main' # entry_point นี้ถูกต้องแล้ว
+            'laser_to_sonar = my_robot_description.laser_to_sonar_node:main'
         ],
     },
 )
